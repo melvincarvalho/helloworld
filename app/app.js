20fbca92f48e3627bc6950483cedcbe327ec5505
Melvin Carvalho <melvincarvalho@gmail.com>
2015-08-28 02:34:34 +0200

    add comments

--- app/app.js
+++ app/app.js
@@ -1,53 +1,70 @@
 var AUTHENDPOINT = "https://databox.me/";
 
+/**
+* The main app
+*/
 var App = angular.module('HelloWorld', [
-    'lumx'
+  'lumx'
 ]);
 
 App.controller('Main', function($scope, $http, $timeout, LxNotificationService, LxProgressService, LxDialogService) {
 
-    // save app configuration if it's the first time the app runs
-    $scope.initApp = function() {
-      $scope.init();
-    };
+  /**
+  * Initialize app
+  */
+  $scope.initApp = function() {
+    $scope.init();
+  };
 
-    $scope.TLSlogin = function() {
-        $scope.loginTLSButtonText = 'Logging in...';
-        $http({
-          method: 'HEAD',
-          url: AUTHENDPOINT,
-          withCredentials: true
-        }).success(function(data, status, headers) {
-          // add dir to local list
-          var user = headers('User');
-          if (user && user.length > 0 && user.slice(0,4) == 'http') {
-            LxNotificationService.success('Login Successful!');
-            $scope.loggedIn = true;
-            $scope.user = user;
-          } else {
-            LxNotificationService.error('WebID-TLS authentication failed.');
-            console.log('WebID-TLS authentication failed.');
-          }
-          $scope.loginTLSButtonText = 'Login';
-        }).error(function(data, status, headers) {
-            LxNotificationService.error('Could not connect to auth server: HTTP '+status);
-            console.log('Could not connect to auth server: HTTP '+status);
-            $scope.loginTLSButtonText = 'Login';
-        });
-    };
+  /**
+  * TLS Login with WebID
+  */
+  $scope.TLSlogin = function() {
+    $scope.loginTLSButtonText = 'Logging in...';
+    $http({
+      method: 'HEAD',
+      url: AUTHENDPOINT,
+      withCredentials: true
+    }).success(function(data, status, headers) {
+      // add dir to local list
+      var user = headers('User');
+      if (user && user.length > 0 && user.slice(0,4) == 'http') {
+        LxNotificationService.success('Login Successful!');
+        $scope.loggedIn = true;
+        $scope.user = user;
+      } else {
+        LxNotificationService.error('WebID-TLS authentication failed.');
+        console.log('WebID-TLS authentication failed.');
+      }
+      $scope.loginTLSButtonText = 'Login';
+    }).error(function(data, status, headers) {
+      LxNotificationService.error('Could not connect to auth server: HTTP '+status);
+      console.log('Could not connect to auth server: HTTP '+status);
+      $scope.loginTLSButtonText = 'Login';
+    });
+  };
 
-    $scope.logout = function() {
-      $scope.init();
-      LxNotificationService.success('Logout Successful!');
-    };
+  /**
+  * Logout
+  */
+  $scope.logout = function() {
+    $scope.init();
+    LxNotificationService.success('Logout Successful!');
+  };
 
-    // set init variables
-    $scope.init = function() {
-        $scope.initialized = true;
-        $scope.loggedIn = false;
-        $scope.loginTLSButtonText = "Login";
-    };
+  /**
+  * Initialize
+  */
+  $scope.init = function() {
+    $scope.initialized = true;
+    $scope.loggedIn = false;
+    $scope.loginTLSButtonText = "Login";
+  };
 
-    $scope.initApp();
+
+  /**
+  * Main
+  */
+  $scope.initApp();
 
 });
